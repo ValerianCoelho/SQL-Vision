666f4299e1ed2649553acadc595b56149608d83a
Valerian Coelho <valerian27012004@gmail.com>
2023-08-28 20:13:08 +0530

    Raw Data Processed

--- SQL_Vision.py
+++ SQL_Vision.py
@@ -2,7 +2,20 @@ import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
+def remove_garbage(text, garbage_list):
+    for char in garbage_list:
+        text = text.replace(char, '')
+    return text.replace('. ', ' ')
+
 img = Image.open('./Image_Test_1.png')
 data = tess.image_to_string(img)
+
+data = remove_garbage(data, '=#«»|')
+data = data.split('\n')
+data = list(filter(lambda x: x!='', data))
+
+for i in range(len(data)):
+    data[i] = data[i].split(' ')
+    data[i] = list(filter(lambda x: x!='', data[i]))
 
 print(data)
